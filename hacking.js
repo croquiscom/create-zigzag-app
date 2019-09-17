3fcd297b975c8cd4ebf33b78e40bf604d90386e7
Troy Tae <tjy970721@gmail.com>
2019-09-17 11:03:32 +0900

    feat: copy repository

--- hacking.js
+++ hacking.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 
 const name = 'create-zigzag-app';
+const fs = require('fs-extra');
 const inquirer = require('inquirer');
 
 inquirer
   .prompt({
     name,
     type: 'list',
-    message: '고르세요. Choose.',
+    message: '고르세요. Choose..',
     choices: [
       'Vanilla',
       'React',
@@ -18,4 +19,7 @@ inquirer
   })
   .then((v) => {
     console.log(v[name]);
+    fs.copy(__dirname, `${process.cwd()}/${name}`)
+      .then(() => console.log('성공했어요! Success!!'))
+      .catch(console.error);
   });
