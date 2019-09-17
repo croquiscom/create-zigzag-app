4878847c5a50a4c2cefbd3a2022d81acf599a984
Troy Tae <tjy970721@gmail.com>
2019-09-17 15:17:59 +0900

    fix: update command script

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ inquirer
   .prompt({
     name,
     type: 'list',
-    message: '고르세요. Choose..',
+    message: 'Select one!',
     choices: [
       'vanilla',
       'react',
@@ -19,7 +19,17 @@ inquirer
     ]
   })
   .then((value) => {
-    fs.copy(__dirname, path.resolve(process.cwd(), value[name]))
-      .then(() => console.log('성공했어요! Success!!'))
-      .catch(console.error);
+    const target = value[name];
+    switch (target) {
+      case 'vanilla':
+        fs.copy(
+            path.resolve(__dirname, target),
+            path.resolve(process.cwd(), target)
+          )
+          .then(() => console.log('Success!!'))
+          .catch(console.error);
+        break;
+      default:
+        console.log(`아직 개발이 안됐어요... ${target} 버전 개발해주세요 :)`);
+    }
   });
