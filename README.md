c52bdce9363360f137440fa7b71edb828f2d0850
Troy Tae <tjy970721@gmail.com>
2019-09-17 15:09:14 +0900

    feat: README.md

---
+++ README.md
@@ -0,0 +1,33 @@
+# create-zigzag-app
+
+## description
+
+Create template project used in ZIGZAG with no build configuration.
+This project works on wherever npm is installed.
+(npx comes with npm 5.2+ and higher, if you use lower version, please update that!)
+
+## How to use
+
+### Step1
+
+Execute this command:
+
+```
+npx git+https://github.com/croquiscom/create-zigzag-app.git
+```
+
+### Step2
+
+Select what you want!
+
+```
+❯ Vanilla 
+  React 
+  React + MaterialUI 
+  Next.js 
+  Next.js + MaterialUI 
+```
+
+### Step3
+
+Enjoy :)
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const name = 'create-zigzag-app';
 const fs = require('fs-extra');
+const path = require('path');
 const inquirer = require('inquirer');
 
 inquirer
@@ -10,15 +11,15 @@ inquirer
     type: 'list',
     message: '고르세요. Choose..',
     choices: [
-      'Vanilla',
-      'React',
-      'React + MaterialUI',
-      'Next.js',
-      'Next.js + MaterialUI'
+      'vanilla',
+      'react',
+      'react-material-ui',
+      'nextjs',
+      'nextjs-material-ui'
     ]
   })
   .then((value) => {
-    fs.copy(__dirname, `${process.cwd()}/${value[name]}`)
+    fs.copy(__dirname, path.resolve(process.cwd(), value[name]))
       .then(() => console.log('성공했어요! Success!!'))
       .catch(console.error);
   });
